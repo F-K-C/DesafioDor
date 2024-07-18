79c5aca0825242bdd4aa1b3c264a30a0120dd14c
F-K-C <fakocarvalho@gmail.com>
2024-07-18 20:18:23 +0000

    CEP via variável

--- dor-cep/src/Cep.jsx
+++ dor-cep/src/Cep.jsx
@@ -1,18 +1,30 @@
+import React, { useState } from "react"
 
 function Cep() {
+    const [cep, setCep] = useState('')
+    const [address, setAddress] = useState(null)
+    const [error, setError] = useState(null)
 
     const getAddress = async () =>{
         try {
-            const response = await fetch('https://ws.apicep.com/cep.json?code=XXX')
+            const response = await fetch(`https://ws.apicep.com/cep.json?code=${cep}`)
             const data = await response.json()
-            console.log(data)
+            
+            if(data.erro){
+                setError('CEP não encontrado')
+                setAddress(null)
+            }else {
+                setAddress(data)
+                setError(null)
+            }
         } catch (error) {
-            console.log(error)
+            setError('Erro ao buscar os dados, tente novamente!')
+            setAddress(null)
         }        
     }
 
     const handleInputChange = (event) =>{
-        console.log(event.target.value)
+        setCep(event.target.value)
     }
 
     return (
