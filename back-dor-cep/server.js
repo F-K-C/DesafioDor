70ec126f9445ef5e3c9e800ea6d5cc0920f8bbba
F-K-C <fakocarvalho@gmail.com>
2024-07-19 18:52:54 +0000

    Integrando front e back

--- back-dor-cep/server.js
+++ back-dor-cep/server.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 app.use(cors())
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Ouvindo na porta:${PORT}`))
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.get('/', (req, res) =>{
     res.send('OK')
+})
+
+// Endpoint para buscar o endereço pelo CEP
+app.post('/buscar-endereco', async (req, res) => {
+    const { cep } = req.body;
+  
+    try {
+      const response = await fetch(`https://ws.apicep.com/cep.json?code=${cep}`);
+      const data = await response.json();
+  
+      if (data.error) {
+        res.status(404).json({ error: 'CEP não encontrado' });
+      } else {
+        res.json({
+          address: data.address,
+          district: data.district,
+          city: data.city,
+          state: data.state,
+          code: data.code
+        });
+      }
+    } catch (error) {
+      console.error('Erro ao buscar endereço:', error.message);
+      res.status(500).json({ error: 'Erro ao buscar endereço' });
+    }
+  });
+  
+app.listen(PORT, () => {
+    console.log(`Ouvindo na porta:${PORT}`)
 })
--- front-dor-cep/src/Cep.jsx
+++ front-dor-cep/src/Cep.jsx
@@ -63,3 +63,90 @@ function Cep() {
 }
 
 export default Cep
+
+
+/*
+import React, { useState } from 'react';
+
+const Cep = () => {
+  const [cep, setCep] = useState('');
+  const [address, setAddress] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/buscar-endereco', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cep }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao buscar endereço');
+      }
+
+      const data = await response.json();
+      setAddress(data);
+      setError(null);
+    } catch (error) {
+      console.error('Erro ao buscar endereço:', error);
+      setError('Erro ao buscar os dados, tente novamente!');
+      setAddress(null);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setCep(event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="texto">Digite o CEP abaixo</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="tracking"
+            className="form-control"
+            value={cep}
+            onChange={handleInputChange}
+          />
+          <button type="submit" name="trackCEP" className="btn btn-primary">
+            Enviar
+          </button>
+        </div>
+      </form>
+
+      {error && <p>{error}</p>}
+
+      {address && (
+        <div className="list-group">
+          <br />
+          <li className="list-group-item">
+            <strong>Rua:</strong> {address.address}
+          </li>
+          <li className="list-group-item">
+            <strong>Bairro:</strong> {address.district}
+          </li>
+          <li className="list-group-item">
+            <strong>Cidade:</strong> {address.city}
+          </li>
+          <li className="list-group-item">
+            <strong>Estado:</strong> {address.state}
+          </li>
+          <li className="list-group-item">
+            <strong>CEP:</strong> {address.code}
+          </li>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cep;
+*/
